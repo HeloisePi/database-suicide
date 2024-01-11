579acd681440e8511f0fa67e11c6a39ea71620f0
Héloïse P <heloise.pingitore@mmibordeaux.com>
2024-01-11 09:19:57 +0100

    Add css, typo, content

---
+++ src/routes/componant/Articles.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let imgSrc;
+    export let imgAlt;
+
+</script>
+
+
+<section>
+    <div class="images">
+        <h3>suicide post-covid</h3>
+        <img src="{imgSrc}" alt="{imgAlt}">
+    </div>
+
+    <div class="">
+        <p><slot></slot></p>
+    </div>
+
+</section>
+
+
+<style>
+    section{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .images{
+        border: 2px black dashed;
+        width: 30%;
+        padding: 1rem;
+    }
+    img{
+        width: -webkit-fill-available;
+        height:-webkit-fill-available;
+    }
+
+</style>
